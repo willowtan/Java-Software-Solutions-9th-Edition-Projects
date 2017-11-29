e1fa0b5f3185663f18c9271be2ffc7ef08fc1054
Alex Lynd <alexlynd@protonmail.com>
2017-11-29 10:13:04 -0800

    Add files via upload

---
+++ Chapter 4/4.2/Lights.java
@@ -0,0 +1,11 @@
+public class Lights { //make class Lights
+ public static void main(String[]args){ //create a void main
+  Bulb bulb1 = new Bulb (true); //make new Bulb object bulb1 set to true
+  Bulb bulb2 = new Bulb (false); //make new Bulb object bulb2 set to false
+  System.out.println ("Bulb 1: " +bulb1.state()); //print the state of the bulb
+  System.out.println ("Bulb 2: " +bulb2.state()); 
+  bulb1.Off(); //set the state of the bulb
+  bulb2.On(); //set the state of the bulb
+  System.out.println ("Bulb 1: " +bulb1.state()); 
+  System.out.println ("Bulb 2: " +bulb2.state()); 
+ }} 

f9cd106ffe724c286aaf592e909b0984097e2f1c
Alex Lynd <alexlynd@protonmail.com>
2017-11-29 13:34:19 -0800

    Update Websites.java

--- Chapter 1/Websites.java
+++ Chapter 1/Websites.java
@@ -1,5 +1,7 @@
 /* Write a program that prints a list of four or five Web sites
  that you enjoy. Print both the site name and the URL. */
+//Project 1.4
+
 public class Websites
 {
   public static void main (String [] args){
@@ -7,6 +9,5 @@ public class Websites
       System.out.println("Cemetech- https://www.cemetech.net/");
       System.out.println("Github- https://github.com/");
       System.out.println("Z80 Ref.- http://www.z80.info/");
-      System.out.println("Ticalc- http://www.ticalc.org/");
-    }
-}
+      System.out.println("Ticalc- http://www.ticalc.org/");  
+}}

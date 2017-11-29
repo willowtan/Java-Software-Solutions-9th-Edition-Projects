3a04a5ce95ea0dda8df814c2ba04083783f23bf6
Alex Lynd <alexlynd@protonmail.com>
2017-11-29 13:32:50 -0800

    Add files via upload

---
+++ Chapter 1/Tree.java
@@ -0,0 +1,10 @@
+/*Write a program that prints the outline of a tree using asterisk (*) characters.*?
+//Project 1.6
+
+
+public class Tree
+{
+  public static void main (String [] args){
+      System.out.println("  *\n ***\n*****\n  *");
+
+   }}
---
+++ Chapter 1/Websites.java
@@ -0,0 +1,12 @@
+/* Write a program that prints a list of four or five Web sites
+ that you enjoy. Print both the site name and the URL. */
+public class Websites
+{
+  public static void main (String [] args){
+      System.out.println("Hackaday- https://bpb.opendns.com/a/hackaday.com/");
+      System.out.println("Cemetech- https://www.cemetech.net/");
+      System.out.println("Github- https://github.com/");
+      System.out.println("Z80 Ref.- http://www.z80.info/");
+      System.out.println("Ticalc- http://www.ticalc.org/");
+    }
+}

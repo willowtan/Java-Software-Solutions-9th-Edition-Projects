e3e38c508b31822aa7247c297bacc4c91cd7e8ff
Alex Lynd <alexlynd@protonmail.com>
2017-11-29 18:30:58 -0800

    Added comments

--- Chapter 2/Contact.java
+++ Chapter 2/Contact.java
@@ -6,18 +6,19 @@
  */
 //Project 2.4
 
-import java.util.Scanner;
-public class Contact{
-    public static void main(String [] args){
-        Scanner scan=new Scanner (System.in);
-        String name, age, college, petname;
-        System.out.println("What is your name:");
-        name=scan.nextLine();       
+import java.util.Scanner; //import Scanner object
+public class Contact{ //create class Contact
+    public static void main(String [] args){ //make main void method
+        Scanner scan=new Scanner (System.in); //make new Scanner object Scanner
+        String name, age, college, petname; // declare variables as string
+        System.out.println("What is your name:"); //Prints out a prompt
+        name=scan.nextLine(); //reads input and stores to variable
         System.out.println("How old are you?: "); 
-        age=scan.nextLine();        
+        age=scan.nextLine();
         System.out.println("Where do you want to go to college: ");
-        college=scan.nextLine();        
+        college=scan.nextLine();
         System.out.println("What is your petname:");
-        petname=scan.nextLine();        
+        petname=scan.nextLine();
         System.out.println("Hello, my name is "+name+" and I am "+age+" years old.  I'm my time at  "+college+", though I miss my pet "+petname+" very much!");
-    }}
+        //Prints out final paragraph with the inputted variables
+}}

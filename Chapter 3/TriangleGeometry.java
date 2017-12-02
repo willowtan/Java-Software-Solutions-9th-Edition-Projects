1eb7e8eaa8d9dbb476d1921ef2bb1d545486d4ca
Alex Lynd <alexlynd@protonmail.com>
2017-12-01 17:58:39 -0800

    Add files via upload

---
+++ Chapter 3/TriangleGeometry.java
@@ -0,0 +1,15 @@
+/* Write a program that generates a random integer in the range 20
+ * to 40, inclusive, and displays the sine, cosine, and tangents of
+  that number. */
+//Project 3.8
+
+import java.util.Random;
+public class TriangleGeometry {
+    public static void main(String[]args)     {
+        Random rand= new Random();
+        int random= rand.nextInt(21)+20;
+        System.out.println(random);
+        System.out.println("Sin("+random+"}= "+Math.sin(random));
+        System.out.println("Cos("+random+"}= "+Math.cos(random));
+        System.out.println("Tan("+random+"}= "+Math.tan(random));
+    }}
